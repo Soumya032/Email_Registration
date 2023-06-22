4f28fc85a8075045e6b63b7b9c8fb67928c824ad
soumya032 <soumyadash470@gmail.com>
2023-06-23 00:36:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.encodeURI({extended:true}));
+
+
+app.get("/",(req,res) => {
+   res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/",(req,res) => {
+    const firstName=req.body.first;
+    const lastName=req.body.last;
+    const email = req.body.email;
+    console.log(firstName +" " +lastName);
+
+
+    const data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                     FNAME:firstName,
+                     LNAME:lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData =JSON.stringify(data);
+
+    const url="https://us21.api.mailchimp.com/3.0/lists/2e8f909bdb";
+
+
+    const options={
+        method:"POST",
+        auth:"soumya:690c3123b64c5f84be048fae2d2d1982-us21"
+    }
+
+    const req_est=https.request(url,options,(response) => {
+
+/*           if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+          }
+          else{
+            res.sendFile(__dirname+"/failure.html");
+          } */
+          response.on("data",(data) => {
+            const dataObject = JSON.parse(data);
+            if(dataObject.errors[0].error_code==="ERROR_GENERIC"){
+                res.sendFile(__dirname+"/failure.html");
+            }
+            else if(response.statusCode===200){
+                res.sendFile(__dirname+"/success.html");
+            }
+            else{
+                res.sendFile(__dirname+"/failure.html");
+            }
+            console.log(dataObject);
+          })
+                    
+    })
+    req_est.write(jsonData);
+    req_est.end();
+})
+
+
+app.post("/failure",(req,res) => {
+    res.redirect("/");
+})
+
+app.listen(3000,() => {
+    console.log("server stared at port 3000");
+})
